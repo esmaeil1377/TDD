2a42d33b9853e815133c265f5ceccd6e7f120201
sh3rLock3d <alipourghasemi78@gmail.com>
2021-11-02 22:12:29 +0330

    second part

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -1,8 +1,24 @@
 package main;
 public class Rectangle {
-    private static int width = 3;
-    private static int height = 4;
-    public static int computeArea() {
+    private static double width;
+    private static double height ;
+    public static double computeArea() {
         return width * height;
+    }
+
+    public static double getWidth() {
+        return width;
+    }
+
+    public static void setWidth(double width) {
+        Rectangle.width = width;
+    }
+
+    public static double getHeight() {
+        return height;
+    }
+
+    public static void setHeight(double height) {
+        Rectangle.height = height;
     }
 }
--- src/test/RectangleAreaTest.java
+++ src/test/RectangleAreaTest.java
@@ -7,7 +7,22 @@ import org.junit.Test;
 public class RectangleAreaTest {
 
     @Test
-    public void testRectangleArea() {
-        assertEquals(12, Rectangle.computeArea() );
+    public void testRectangleAreaInt() {
+        Rectangle.setHeight(5);
+        Rectangle.setWidth(4);
+        assertEquals(20, Rectangle.computeArea(), 0.01);
+        Rectangle.setHeight(6);
+        Rectangle.setWidth(7);
+        assertEquals(42, Rectangle.computeArea(), 0.01);
+    }
+
+    @Test
+    public void testRectangleAreaDouble() {
+        Rectangle.setHeight(5.5);
+        Rectangle.setWidth(4.5);
+        assertEquals(24.75, Rectangle.computeArea(), 0.01);
+        Rectangle.setHeight(6.6);
+        Rectangle.setWidth(7.3);
+        assertEquals(48.18, Rectangle.computeArea(), 0.01);
     }
 }
